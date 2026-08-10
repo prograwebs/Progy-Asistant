e5140363068f29bb404869bdc927af742d3e1b28
harold <152220718+Haroldhv4@users.noreply.github.com>
2026-08-10 17:44:46 -0500

    Add Cloudflare binding type declarations

---
+++ types/cloudflare.d.ts
@@ -0,0 +1,17 @@
+declare module "cloudflare:workers" {
+  export const env: {
+    DB?: D1Database;
+    [key: string]: unknown;
+  };
+}
+
+interface Fetcher {
+  fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
+}
+
+interface D1Database {
+  prepare(query: string): unknown;
+  batch<T = unknown>(statements: unknown[]): Promise<T[]>;
+  exec(query: string): Promise<unknown>;
+  dump(): Promise<ArrayBuffer>;
+}
